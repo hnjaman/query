fed026ebb2fb9760c2cd3ce5943844d7fde981be
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-12-04 14:16:32 +0600

    comment and git

--- June 18.sql
+++ June 18.sql
@@ -1,16 +1,17 @@
-http://103.94.135.151:8080/JGTDSL_WEB/getiPgDetailInfo.action?customerId=020110002
+--http://103.94.135.151:8080/JGTDSL_WEB/getiPgDetailInfo.action?customerId=020110002
 
-[pubali bank]
+--[pubali bank]
+
 update bank_account_ledger set branch_id ='10211431', account_no='10211431' where trans_id in (
 select trans_id from bank_account_ledger where --BRANCH_ID = '10211431' -- 10211412
  TRANS_DATE between TO_DATE('08/2018', 'MM/YYYY') and TO_DATE('08/31/2018', 'MM/DD/YYYY')
 and INSERTED_BY='Assistant Manager Shayestagonj'
 and customer_id like '13%'
 and bank_id ='102114'
-)
+);
 
 
-[ merge from diff table]
+--[ merge from diff table]
 
 MERGE INTO bill_non_metered bnm
      USING (SELECT *
@@ -25,9 +26,9 @@ THEN
               COLLECTED_PAYABLE_AMOUNT = ACTUAL_PAYABLE_AMOUNT,
               status = 2,
               BILL_TYPE = 3
-           WHERE bnm.status = 1
+           WHERE bnm.status = 1;
 
-[update from deferent table]
+--[update from deferent table]
 
 UPDATE bill_non_metered bnm
    SET (COLLECTION_DATE) = (SELECT BCA.TRANS_DATE
@@ -46,36 +47,36 @@ UPDATE bill_non_metered bnm
     SELECT 1
       FROM bill_coll_advanced bca
       WHERE bnm.bill_id = BCA.BILL_ID 
-      AND BCA.BILL_ID = '201810010119907')
+      AND BCA.BILL_ID = '201810010119907');
 
-[ update from deferent table]
+--[ update from deferent table]
 
 MERGE INTO customer_personal_info cpi
      USING CUSTOMER_PHONE cp
         ON (CPI.CUSTOMER_ID = CP.CUSTOMER_ID)
 WHEN MATCHED
 THEN
-   UPDATE SET cpi.MOBILE = cp.MOBILE_NEW
+   UPDATE SET cpi.MOBILE = cp.MOBILE_NEW;
 
-[ Remove characters from a mobile number]
+--[ Remove characters from a mobile number]
 
-update CUSTOMER_PHONE set mobile=REGEXP_REPLACE(mobile,'[^[:alnum:]'' '']', NULL) where  length(mobile)>=12
-update CUSTOMER_PHONE set mobile=REGEXP_REPLACE(mobile, '[^0-9]+', '') where  length(mobile)>11
+update CUSTOMER_PHONE set mobile=REGEXP_REPLACE(mobile,'[^[:alnum:]'' '']', NULL) where  length(mobile)>=12;
+update CUSTOMER_PHONE set mobile=REGEXP_REPLACE(mobile, '[^0-9]+', '') where  length(mobile)>11;
 
-[ fraction value ]
+--[ fraction value ]
 
 select customer_id,double_burner,floor(double_burner) int1,trunc(double_burner) int2,
 double_burner - floor(double_burner) fr1,double_burner - trunc(double_burner) fr2--,mod(wt,sign(wt)) fr3,remainder(wt,sign(wt)) fr4
 FROM bill_non_metered
- WHERE bill_id LIKE '201806%'
+ WHERE bill_id LIKE '201806%';
 
-[ duplicate meter delete]
+--[ duplicate meter delete]
 
 delete from customer_meter where (customer_id, meter_id) in(
 select customer_id, min(meter_id)  from customer_meter  where customer_id like '0410%'
-group by customer_id having count(*)>1)
+group by customer_id having count(*)>1);
 
-[ Delete duplicate data]
+--[ Delete duplicate data]
 
 DELETE sales_report
 WHERE rowid in
@@ -89,7 +90,7 @@ WHERE rowid in
                having count(BILL_ID)>1);
 
 			   
-[ duplicate data show]
+--[ duplicate data show]
 
 SELECT *
   FROM BG_DELETED
@@ -97,9 +98,9 @@ SELECT *
           (  SELECT TOTAL_DEPOSIT
                FROM BG_DELETED
            GROUP BY TOTAL_DEPOSIT
-             HAVING COUNT (*) > 1)
+             HAVING COUNT (*) > 1);
 
-[ 2nd highest value ]
+--[ 2nd highest value ]
 
 SELECT *
   FROM (SELECT pid2.pid, ROWNUM rnum
@@ -115,14 +116,14 @@ SELECT MAX (pid)
  WHERE     APPLIANCE_TYPE_CODE NOT IN ('01', '02')
        AND pid <> (SELECT MAX (pid)
                      FROM BURNER_QNT_CHANGE
-                    WHERE APPLIANCE_TYPE_CODE NOT IN ('01', '02'))
+                    WHERE APPLIANCE_TYPE_CODE NOT IN ('01', '02'));
 ---------------------------------------
 SELECT MAX (TOTAL_DEPOSIT)
   FROM BG_DELETED
  WHERE total_deposit NOT IN (SELECT MAX (TOTAL_DEPOSIT)
-                               FROM BG_DELETED)
+                               FROM BG_DELETED);
 
-[ Delete demand note bill ]
+--[ Delete demand note bill ]
 
 delete from BILL_NON_METERED where bill_id in ((SELECT 201801||customer_id bill_id
    FROM bill_non_metered
